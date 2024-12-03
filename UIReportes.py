77a7803ec3d6784dbd96285fc01c22de06131a2c
PedroJesus269 <pedroahuanlla7@gmail.com>
2024-12-02 23:08:05 -0500

    Update UIReportes.py

--- UIReportes.py
+++ UIReportes.py
@@ -193,14 +193,14 @@ def generate_daily_report(caution_df, alarm_df, report_date):
                         fontsize=9, color='black', rotation=90)
 
     ax.set_yticks([])
-    ax.set_title(f'{report_date} - Sensores Mina', fontsize=16, pad=20, loc='left')
+    ax.set_title(f'{report_date} - Sensores Challcobamba', fontsize=16, pad=20, loc='left')
 
     # Filtrar solo las alertas de interés para la leyenda
     legend_status = ['Red', 'Yellow', 'Grey']
     legend_patches = [
         mpatches.Patch(color=color, label='Alerta Roja' if status == 'Red' else 
                                         'Alerta Amarilla' if status == 'Yellow' else 
-                                        'Libre entre Alertas <=1hr' if status == '' else status) 
+                                        'Libre entre Alertas <=1hr' if status == 'Grey' else status) 
         for status, color in status_colors.items() if status in legend_status
     ]
 
@@ -284,7 +284,7 @@ def plot_eventos(df, report_date):
     # Etiquetas y título
     ax.set_xlabel('Horas del día')
     ax.set_ylabel('Eventos')
-    ax.set_title(f'Frecuencia de descargas eléctricas por hora del día {report_date}\nSensor 2 Mirador Evelyn', fontsize=16, pad=20)
+    ax.set_title(f'Frecuencia de descargas eléctricas por hora del día {report_date}\nSensores Challcobamba', fontsize=16, pad=20)
     ax.set_xticks(x)
     ax.set_xticklabels([f'{h:02d}:00' for h in range(24)])
 
@@ -461,7 +461,7 @@ def generate_report(df, file_name):
     header_table.columns[0].width = Pt(50)
 
     cell_logo = header_table.cell(0, 0)
-    cell_logo.paragraphs[0].add_run().add_picture("images/logo_doc.PNG", width=Pt(100))
+    cell_logo.paragraphs[0].add_run().add_picture("logo_doc.PNG", width=Pt(100))
 
     cell_title = header_table.cell(0, 1)
     header_table.cell(0, 1).width = Pt(1250)
@@ -494,7 +494,7 @@ def generate_report(df, file_name):
 
     # Configurar el contenido del footer
     footer_paragraph.text = (
-        f"{today_date} Supervisión de Mantenimiento en Telecomunicaciones"
+        f"{today_date} Supervisión de Mantenimiento Eléctrico-Telecomunicaciones"
     )
     footer_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER  # Centrar el texto
 
@@ -541,14 +541,14 @@ def cargar_archivo():
 
 # Mostrar el logo (asegúrate de tener el archivo en la ruta correcta)
 try:
-    imagen = Image.open("images/logo.png")
+    imagen = Image.open("logo.png")
     imagen = imagen.resize((100, 50), Image.Resampling.LANCZOS)
     st.image(imagen, use_container_width=False)
 except FileNotFoundError:
     st.warning("No se encontró la imagen en la ruta especificada.")
 
 # Título largo de la app
-st.title("Generador de Reportes Diarios de Alertas por Descargas Eléctricas Atmosféricas")
+st.title("Generador de Reportes Diarios CHALLCOBAMBA")
 
 
 # Llamar a la función para cargar el archivo
