e7fade5ef11341a2d0c252cc3958a4600a4950ed
PedroJesus269 <pedroahuanlla7@gmail.com>
2025-01-13 20:54:41 -0500

    Update UIFerrobamba.py

--- UIFerrobamba.py
+++ UIFerrobamba.py
@@ -10,6 +10,7 @@ from docx.shared import Inches
 from docx.shared import Pt
 from docx import Document
 from io import BytesIO
+from datetime import timedelta
 
 
 from datetime import datetime
@@ -362,8 +363,11 @@ def get_daily_plot(final_data):
     ax.set_yticks([])
 
     # Obtener la fecha DD/MM/YYYY de la segunda columna de date
-    report_date = final_data.iloc[1]['Date'].strftime('%d/%m/%Y')
-    ax.set_title(f'{report_date} - Sensores Mina', fontsize=16, pad=20, loc='left')
+    report_date = final_data.iloc[1]['Date']
+    next_date = report_date + timedelta(days=1)
+    report_date_str = report_date.strftime('%d/%m/%Y')
+    next_date_str = next_date.strftime('%d/%m/%Y')
+    ax.set_title(f'{report_date_str} - {next_date_str} - Sensores Mina', fontsize=16, pad=20, loc='left')
 
     # Remove x-axis label
     ax.set_xlabel('')
