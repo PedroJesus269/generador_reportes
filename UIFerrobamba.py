0358dcbbf9ba2fa3915427e170c8b304dafe0d29
PedroJesus269 <pedroahuanlla7@gmail.com>
2025-01-13 21:56:39 -0500

    Update UIFerrobamba.py

--- UIFerrobamba.py
+++ UIFerrobamba.py
@@ -75,7 +75,7 @@ def plot_eventos(df):
     ax.set_xlabel('Horas del día')
     ax.set_ylabel('Eventos')
     report_date = df.iloc[1]['Start'].strftime('%d/%m/%Y')
-    ax.set_title(f'Frecuencia de descargas eléctricas por hora del día {report_date_str} - {next_date_str}\nSensor 2 Mirador Evelyn', fontsize=16, pad=20)
+    ax.set_title(f'Frecuencia de descargas eléctricas por hora del día {report_}\nSensor 2 Mirador Evelyn', fontsize=16, pad=20)
     ax.set_xticks(x)
     ax.set_xticklabels([f'{(h % 24):02d}:00' for h in range(7, 24)] + [f'{(h % 24):02d}:00' for h in range(0, 7)])
 
@@ -367,7 +367,7 @@ def get_daily_plot(final_data):
     next_date = report_date + timedelta(days=1)
     report_date_str = report_date.strftime('%d/%m/%Y')
     next_date_str = next_date.strftime('%d/%m/%Y')
-    ax.set_title(f'Frecuencia de descargas eléctricas por hora del día {report_date_str} - {next_date_str}\nSensor 2 Mirador Evelyn', fontsize=16, pad=20)
+    ax.set_title(f'Frecuencia de descargas eléctricas por hora del día\nSensor 2 Mirador Evelyn', fontsize=16, pad=20)
 
     # Remove x-axis label
     ax.set_xlabel('')
